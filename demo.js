864a972da6ccf109ca620df1d28d7c01ff259f43
Abhijeet Kawhale <abhijeet.kawhale@gmail.com>
2023-12-26 09:00:59 +0530

    variable Add
    variable Add

---
+++ demo.js
@@ -0,0 +1,26 @@
+console.log("Hello World, I am Abhijeet");
+
+var num;
+num="10";
+console.log(num);
+var myName="Abhijeet Kawhale";
+console.log("full name", myName);
+var myWifename = null;
+console.log(myWifename);
+
+
+
+
+console.log("Hello JS , I am Gajanan");
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Gajanan"; // Variable Intialization
+console.log("First Name: ",myName);
+
+var age = "32"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Gajanan Kharat"; // Varibale update
+console.log("Full name: ", myName);
